8d2ccc3319cc6295bcf6638eff57980b16559148
Jua Jang <jua.jang@kakao.com>
2020-12-04 13:18:41 +0900

    add Animated Circles with transition

--- src/pages/ReactWithD3Basic.jsx
+++ src/pages/ReactWithD3Basic.jsx
@@ -7,6 +7,7 @@ import { Divider } from "antd";
 import Mark from "../components/common/Mark";
 import RandomCirclesUsingReact from "../components/basic/RandomCircleUsingReact";
 import RandomCircles from "../components/basic/RandomCircles";
+import AnimatedCircle from '../components/basic/AnimatedCircle';
 
 const Wrapper = styled.div`
   display: grid;
@@ -71,18 +72,26 @@ const ReactWithD3Basic = () => {
       <Divider/>
       <Title> Random circles using .join() method </Title>
       <p>
-        We're re-running our code whenever <Mark> dataset changes </Mark> <br/>
-        Use <Mark> useInterval </Mark> to re-calculate our <Mark> dataset </Mark>
+        <Mark> dataset </Mark>이 변할때마다 코드를 다시 실행합니다. <br/>
+        이때 <Mark> dataset </Mark>을 다시 계산하기 위해서 <Mark> useInterval </Mark>을 사용합니다.
       </p>
       <RandomCircles/>
       <Divider/>
       <Title> Random circles using React </Title>
       <p>
-        Looping over each data point, and <br/>
-        rendering a <Mark> circle </Mark> at <Mark> [x, y] </Mark>
+        각각의 data point를 돌면서<br/>
+        <Mark> [x, y] </Mark>에 있는 <Mark> circle </Mark>을 렌더링합니다.
       </p>
       <RandomCirclesUsingReact/>
       <Divider/>
+      <Title> Animated Circles with transition </Title>
+      <p>
+        우리는 6개의 <Mark> circle </Mark>을 가지고 2초마다 랜덤하게 렌더링합니다. <br/>
+        새로운 circle은 파란색으로 표시합니다.  <br/>
+        여러 라운드동안 유지되고 있는 circle은 회색으로 표시합니다.  <br/>
+        새로운 라운드에서 빠지게 되는 circle은 빨간색으로 표시합니다.  <br/>
+      </p>
+      <AnimatedCircle/>
     </Wrapper>
   );
 }
